eaf15642304b41bac72a25ec5b02a5de1e589762
python-developer-algebra-klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-01-31 19:53:25 +0100

    Created main.py and update .gitignore

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Kreirati varijablu za čuvanje podataka o studentu:
+ime i prezime, OIB, email, broj telefona, prosječna ocjena,
+godina upisa studija, naziv studija, naziv sveučilišta
+'''
+first_name = 'Ana'
+last_name = 'Anic'
+VAT_ID = '012345678901'
+email = 'ana.anic@domena.hr'
+phone = '+385 9x 1234 567'
+average_grade = 4.99
+starting_year = 2020
+study_program = 'Racunalno programiranje'
+university = 'Sveuciliste Algebra, Zagreb'
